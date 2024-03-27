57b7376af16bcc1f8f98db50833486c9bd9095ce
Alexander Schiller-Stefansson <alesc@itu.dk>
2024-03-27 16:17:29 +0100

    Layout Switching Code
    Still missing HV-tree root placement code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from Fibonacci_heap.FibonacciHeap import FibonacciHeap
-from Manim5 import FiboScene
+from Manim5 import *
 from manim.utils.file_ops import open_file as open_media_file 
 from manim import config
 from manim import *
@@ -10,10 +10,10 @@ config.max_files_cached = 300
 config.disable_caching = True
 config.disable_caching_warning = True
 
-config.quality = "low_quality"
+#config.quality = "low_quality"
 #config.quality = "medium_quality"
 #config.quality = "high_quality"
-#config.quality = "production_quality"
+config.quality = "production_quality"
 #config.quality = "fourk_quality"
 
 def run():
@@ -25,15 +25,22 @@ def run():
 
     #Set animation 
     heap.isAnimation = False
-    for i in range(25):
+    for i in range(32):
         heap.insert(i)
-    #heap.extract_min()
-    #heap.decrease_value(20, 0)
-    #heap.extract_min()
+
+    heap.isAnimation = True
+    heap.insert(99)
+
+    scene.changeTreeLayout(TreeLayout.H_V)
 
     heap.extract_min()
-    heap.isAnimation = True
+    
+    scene.changeTreeLayout(isAnimation=True)
     heap.insert(0)
+
+    
+
+
 
     scene.render()
     print("--- %s seconds ---" % (time.time() - start_time))
