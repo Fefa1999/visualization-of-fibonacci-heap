08fb832217aaac444e156b1da15d28b562dca057
Alexander Schiller-Stefansson <alesc@itu.dk>
2024-04-01 21:02:45 +0200

    90% implementation of layout sensitive

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ config.max_files_cached = 300
 config.disable_caching = True
 config.disable_caching_warning = True
 
-#config.quality = "low_quality"
+config.quality = "low_quality"
 #config.quality = "medium_quality"
 #config.quality = "high_quality"
-config.quality = "production_quality"
+#config.quality = "production_quality"
 #config.quality = "fourk_quality"
 
 def run():
@@ -25,22 +25,22 @@ def run():
 
     #Set animation 
     heap.isAnimation = False
-    for i in range(32):
+
+    scene.changeRootPackingAndSorting(RootPacking.FFDH)
+
+    scene.add(Dot(point=(0, 3, 0), radius=0.4, color=PURPLE))
+
+    for i in range(33):
         heap.insert(i)
 
     heap.isAnimation = True
-    heap.insert(99)
-
-    scene.changeTreeLayout(TreeLayout.H_V)
-
     heap.extract_min()
-    
-    scene.changeTreeLayout(isAnimation=True)
-    heap.insert(0)
 
-    
+    heap.isAnimation = True
+    heap.decrease_value(10, 0)
 
-
+    heap.decrease_value(11, 0)
+    heap.decrease_value(17,0)
 
     scene.render()
     print("--- %s seconds ---" % (time.time() - start_time))
