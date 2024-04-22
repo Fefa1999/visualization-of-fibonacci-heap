0926aae8b75c26608f46aeb9e0f56ddea1fc64aa
Fefa1999 <fefa@itu.dk>
2024-04-22 17:21:50 +0200

    first additions, decrease key doesn't work yet

--- Fibonacci_heap/FibonacciHeap.py
+++ Fibonacci_heap/FibonacciHeap.py
@@ -21,29 +21,26 @@ class FibonacciHeap:
         new_node = self.FibonacciHeapNode(value, self.id)
         self.id += 1
         self.merge_node_into_root_list(new_node)
-        if self.scene is not None:
-            self.scene.insert_dot(new_node.value, self.isAnimation, new_node.id)
-        self.check_min_with_single_node(new_node, True)
+        update = self.update_min_with_single_node(new_node)
         self.total_fib_nodes += 1
-        return new_node
+        return (new_node, update)
 
     #updates min if node is smaller than current min
-    def check_min_with_single_node(self, fib_node, print):
+    def update_min_with_single_node(self, fib_node):
         if self.min_fib_node is None or fib_node.value <= self.min_fib_node.value:
             self.min_fib_node = fib_node
-            if print and self.scene is not None:
-                self.scene.set_min(fib_node.id, self.isAnimation)
+            return True
+        else: 
+            return False
    
     #iterates the circular doubly linked list and updates min node
     def set_new_min_from_root_list(self):
         current_node = self.root_list.right
         while True: 
-            self.check_min_with_single_node(current_node, False)
+            self.update_min_with_single_node(current_node)
             if self.root_list == current_node:
                 break
             current_node = current_node.right
-        if self.scene is not None:
-            self.scene.set_min(self.min_fib_node.id, self.isAnimation)
 
     #return min node in O(1) time
     def returnMin(self):
@@ -59,7 +56,7 @@ class FibonacciHeap:
             self.root_list.right = heap_two.root_list.left
             heap_two.root_list.left.left = self.root_list
             self.total_fib_nodes += heap_two.total_fib_nodes
-            self.check_min_with_single_node(heap_two.min_fib_node, True)
+            self.update_min_with_single_node(heap_two.min_fib_node, True)
 
     # insert a node in the circular doubly linked list root_list - will be inserted as new root  
     def merge_node_into_root_list(self, node_to_insert):
@@ -127,26 +124,25 @@ class FibonacciHeap:
 
             #remove min node from root
             self.remove_node_from_root_list(min_node)
-            if self.scene is not None:
-                self.scene.delete(min_node.id, self.isAnimation)
             self.total_fib_nodes -= 1
 
             #Consolidate and set new min unless root_list is only one root or empty - the only child of the removed min
             if self.root_list is not None:
                 if self.root_list != self.root_list.right:
-                    self.consolidate()
+                    actions = self.consolidate()
                 self.set_new_min_from_root_list()
-            return min_node
+            return (min_node, actions, self.min_fib_node)
 
     #Map heap until no root has same degree
     def consolidate(self):
+        actions = []
         max_degree = int(math.log(self.total_fib_nodes, (1 + math.sqrt(5)) / 2)) + 1
         array = [None] * (max_degree + 1)
-        if self.scene is not None:
-            scene_array = self.scene.create_array((max_degree + 1), self.showExplanatoryText)
+        # if self.scene is not None:
+        #     scene_array = self.scene.create_array((max_degree + 1), self.showExplanatoryText)
         array[self.root_list.degree] = self.root_list
-        if self.scene is not None:
-            self.scene.add_number_to_empty_space_in_array(self.root_list.degree, self.root_list.id, scene_array, self.showExplanatoryText, True)
+        # if self.scene is not None:
+        #     self.scene.add_number_to_empty_space_in_array(self.root_list.degree, self.root_list.id, scene_array, self.showExplanatoryText, True)
         end_node = self.root_list.left
         current_node = self.root_list.right
 
@@ -154,27 +150,32 @@ class FibonacciHeap:
             next_node = current_node.right
             if array[current_node.degree] is None:
                 array[current_node.degree] = current_node
-                if self.scene is not None:
-                    self.scene.add_number_to_empty_space_in_array(current_node.degree, current_node.id, scene_array, self.showExplanatoryText, False)
+                # if self.scene is not None:
+                #     self.scene.add_number_to_empty_space_in_array(current_node.degree, current_node.id, scene_array, self.showExplanatoryText, False)
             else:
-                node = self.link_nodes(array[current_node.degree], current_node)
+                nodes = self.link_nodes(array[current_node.degree], current_node)
+                node = nodes[0]
+                actions.append((nodes[0], nodes[1]))
                 array[node.degree-1] = None
-                if self.scene is not None:
-                    self.scene.remove_from_array(node.degree-1, scene_array, self.showExplanatoryText)
+                # if self.scene is not None:
+                #     self.scene.remove_from_array(node.degree-1, scene_array, self.showExplanatoryText)
                 while node.degree <= max_degree-1 and array[node.degree] is not None:
-                    node = self.link_nodes(array[node.degree], node)
+                    nodes = self.link_nodes(array[node.degree], node)
+                    node = nodes[0]
+                    actions.append((nodes[0], nodes[1]))
                     array[node.degree-1] = None
-                    if self.scene is not None:
-                        self.scene.remove_from_array(node.degree-1, scene_array, self.showExplanatoryText)
+                    # if self.scene is not None:
+                    #     self.scene.remove_from_array(node.degree-1, scene_array, self.showExplanatoryText)
                 array[node.degree] = node
-                if self.scene is not None:
-                    self.scene.add_number_to_filled_space_in_array(node.degree, node.id, scene_array, self.showExplanatoryText)
+                # if self.scene is not None:
+                #     self.scene.add_number_to_filled_space_in_array(node.degree, node.id, scene_array, self.showExplanatoryText)
             if current_node == end_node:
-                if self.scene is not None:
-                    self.scene.remove_array(scene_array, self.showExplanatoryText)
+                # if self.scene is not None:
+                #     self.scene.remove_array(scene_array, self.showExplanatoryText)
                 break
             current_node = next_node
 
+        return actions
 
     #Link to nodes toegether - one will become child, other parent 
     def link_nodes(self, fib_node_one, fib_node_two):
@@ -184,58 +185,47 @@ class FibonacciHeap:
             self.merge_node_with_child_list(fib_node_child, fib_node_parent)
             fib_node_child.parent = fib_node_parent
             fib_node_parent.degree += 1
-            if self.scene is not None:
-                self.scene.create_child(fib_node_parent.id, fib_node_child.id, self.isAnimation, self.showExplanatoryText)
-            return fib_node_parent
+            return (fib_node_parent, fib_node_child)
 
     #function to decrease value of a node - eg. 46 -> 12 
-    def decrease_value(self, node_to_decrease_id, new_value):
-        node_to_decrease = self.find_node_by_id(self.root_list, node_to_decrease_id)
+    def decrease_value(self, node_to_decrease, new_value):
         if node_to_decrease.value > new_value:
             node_to_decrease.value = new_value
             parent = node_to_decrease.parent
-            self.scene.change_key(node_to_decrease.id, new_value, self.isAnimation, self.showExplanatoryText)
+            actions = []
             if parent is not None and parent.value > new_value:
-                self.cut(node_to_decrease)
-                self.cascading_cut(parent)
+                cut_node_info = self.cut(node_to_decrease)
+                actions.append(cut_node_info)
+                actions = self.cascading_cut(parent, actions)
 
-            self.check_min_with_single_node(node_to_decrease, True) #TODO fix 2ønd parameter
-
-    def find_node_by_id(self, start_node, id):
-        current_node = start_node
-        while True:
-            if current_node.id == id:
-                return current_node
-
-            if current_node.child is not None:
-                found_node = self.find_node_by_id(current_node.child, id)
-                if found_node is not None:  
-                    return found_node
-
-            if current_node.right == start_node:
-                break  
-            else:
-                current_node = current_node.right
-        return None
+            update = self.update_min_with_single_node(node_to_decrease) #TODO fix 2nd parameter
+            return (True, node_to_decrease, actions, update)
+        else:
+            return (False, node_to_decrease, new_value)
 
     #Cut node from child list to root 
     def cut(self, node_to_cut):
         self.remove_node_from_child_list(node_to_cut)
         self.merge_node_into_root_list(node_to_cut)
-        self.scene.cut(node_to_cut.id, self.isAnimation, node_to_cut.marked, self.showExplanatoryText)
+        mark = node_to_cut.marked
         if node_to_cut.marked:
             node_to_cut.marked = False
-    
+        return (node_to_cut, mark)
+        
     # handle parent of cut node in decreasing a value
-    def cascading_cut(self, decreased_node_parent):
+    def cascading_cut(self, decreased_node_parent, actions):
         if decreased_node_parent.parent is not None:
             if not decreased_node_parent.marked:
                 decreased_node_parent.marked = True
-                self.scene.cascading_cut(decreased_node_parent.id, self.isAnimation, self.showExplanatoryText)
+                actions.append(decreased_node_parent)
+                return actions
             else:
                 next_parent = decreased_node_parent.parent
-                self.cut(decreased_node_parent)
-                self.cascading_cut(next_parent)
+                cut_node_info = self.cut(decreased_node_parent)
+                actions.append(cut_node_info)
+                self.cascading_cut(next_parent, actions)
+        else:
+            return actions
 
     def delete_node(self, node_to_delete):
         self.decrease_value(node_to_delete, -float('inf'))
@@ -287,21 +277,3 @@ class FibonacciHeap:
             if current_node.child is not None:
                 self.recursivePrint(current_node, degree+2)
             current_node = current_node.right
-
-def run():
-    f = FibonacciHeap()
-    for i in range(8):
-        f.insert(i)
-    
-    # g = FibonacciHeap()
-    # for i in range(8):
-    #     g.insert(i+22)
-    
-    # f.extract_min()
-    # g.extract_min()
-
-    # f.merge_heaps(g)
-    # f.extract_min()
-    f.extract_min()
-    f.printHeap()
-run()
--- app.py
+++ app.py
@@ -1,47 +1,27 @@
-from Fibonacci_heap.FibonacciHeap import FibonacciHeap
-from Manim5 import *
-from manim.utils.file_ops import open_file as open_media_file 
-from manim import config
-from manim import *
-
-import time
-
-config.max_files_cached = 300
-config.disable_caching = True
-config.disable_caching_warning = True
-
-
-config.quality = "low_quality"
-#config.quality = "medium_quality"
-#config.quality = "high_quality"
-#config.quality = "production_quality"
-#config.quality = "fourk_quality"
+from controller import Controller
 
 def run():
     #SETUP 
-    start_time = time.time()
-    heap = FibonacciHeap()
-    scene = FiboScene()
-    heap.scene = scene
+    heap_controller = Controller()
+    heap_controller.change_packing()
     
-    # Currently checking
-    scene.changeRootPackingAndSorting(RootPacking.FFDH, rootSorting=RootSorting.Order, isAnimation=False)
+    node_to_decrease = None
+    node_to_decrease_two = None
 
-    #Set animation 
-    heap.isAnimation = True
+    for i in range(25):
+        if i == 14:
+            node_to_decrease = heap_controller.insert_node(i)
+        elif i ==13:
+            node_to_decrease_two = heap_controller.insert_node(i)
+        else:
+            heap_controller.insert_node(i)
 
-    # x = Rectangle(height=scene.bounds[1], width=scene.bounds[0])
-    # x.move_to((0+x.width/2, 0-x.height/2, 0))
-    # scene.add(x)
-    # scene.wait(3)
+    heap_controller.set_show_animations(True)
+    heap_controller.extract_min()
+    heap_controller.set_show_explanatory_text(True)
+    heap_controller.decrease_value(node_to_decrease, -1)
+    heap_controller.decrease_value(node_to_decrease_two, 0)
 
-    for i in range(2):
-        heap.insert(i)
-
-    heap.isAnimation = True
-    heap.extract_min()
-
-    scene.render()
-    print("--- %s seconds ---" % (time.time() - start_time))
-    open_media_file(scene.renderer.file_writer.movie_file_path)
+    heap_controller.export_video()
+     
 run()
